94a1d3daed13b3d8f779b69a4e9c8a9344981354
Vishal <vishal.kuttiyil@gmail.com>
2021-11-05 05:21:16 +0530

    adding adapter pattern

---
+++ src/com/structural/adapter/AdapterPatternTest.java
@@ -0,0 +1,41 @@
+package com.structural.adapter;
+
+public class AdapterPatternTest {
+
+    private static Volt getVolt(SocketAdapter sockAdapter, int i) {
+        switch (i) {
+            case 3:
+                return sockAdapter.get3Volt();
+            case 12:
+                return sockAdapter.get12Volt();
+            case 120:
+            default:
+                return sockAdapter.get120Volt();
+        }
+    }
+
+    private static void testObjectAdapter() {
+        SocketAdapter socketAdapter = new SocketObjectAdapterImpl();
+        Volt v3 = getVolt(socketAdapter, 3);
+        Volt v12 = getVolt(socketAdapter, 12);
+        Volt v120 = getVolt(socketAdapter, 120);
+        System.out.println("v3 volts using Object Adapter=" + v3.getVolts());
+        System.out.println("v12 volts using Object Adapter=" + v12.getVolts());
+        System.out.println("v120 volts using Object Adapter=" + v120.getVolts());
+    }
+
+    private static void testClassAdapter(){
+        SocketAdapter socketAdapter = new SocketClassAdapterImpl();
+        Volt v3 = getVolt(socketAdapter, 3);
+        Volt v12 = getVolt(socketAdapter, 12);
+        Volt v120 = getVolt(socketAdapter, 120);
+        System.out.println("v3 volts using Object Adapter=" + v3.getVolts());
+        System.out.println("v12 volts using Object Adapter=" + v12.getVolts());
+        System.out.println("v120 volts using Object Adapter=" + v120.getVolts());
+    }
+
+    public static void main(String[] args) {
+        testClassAdapter();
+        testObjectAdapter();
+    }
+}
---
+++ src/com/structural/adapter/SocketAdapter.java
@@ -0,0 +1,10 @@
+package com.structural.adapter;
+
+public interface SocketAdapter {
+
+    Volt get120Volt();
+
+    Volt get12Volt();
+
+    Volt get3Volt();
+}
