67de99a13a6f8b12fda6dcab25d642eeb589a26d
Vishal <vishal.kuttiyil@gmail.com>
2021-11-11 07:59:38 +0530

    adding flyway pattern

---
+++ src/com/structural/bridge/GreenColor.java
@@ -0,0 +1,8 @@
+package com.structural.bridge;
+
+public class GreenColor implements Color {
+    @Override
+    public void applyColor() {
+        System.out.println("Green");
+    }
+}
---
+++ src/com/structural/bridge/RedColor.java
@@ -0,0 +1,9 @@
+package com.structural.bridge;
+
+public class RedColor implements Color{
+
+    @Override
+    public void applyColor() {
+        System.out.println("Red");
+    }
+}
